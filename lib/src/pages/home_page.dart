1e570eeddc880288e980463bad00c28c69b6069e
alan <alanarostegui@hotmail.com>
2020-10-27 18:58:22 -0600

    floating action button location

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -25,6 +25,7 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print('hey');
